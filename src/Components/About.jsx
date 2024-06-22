663dfa5437ac2f0d6f73b1c118d34883e38cf137
Jaro <jaroslavfilo95@gmail.com>
2024-06-22 16:32:41 +0200

    favicon added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import "./About.css";
+import "../Scripts/ScrollAnimation.js";
 
 function About() {
   return (
@@ -18,15 +19,31 @@ function About() {
           <img src="https://picsum.photos/400"></img>
         </div>
         <h1>What I use</h1>
-        <div className="skills-container">
-          <img src="free-html-5-1-1175208.webp"></img>
-          <img src="css-icon.webp"></img>
-          <img src="javascript-js-icon-2048x2048-nyxvtvk0.png"></img>
-          <img src="1280px-Sass_Logo_Color.svg.png"></img>
-          <img src="React-icon.svg.png"></img>
-          <img src="Typescript_logo_2020.svg.png"></img>
-          <img src="github.png"></img>
-          <img src="Git-Icon-1788C.png"></img>
+        <div class="logos">
+          <div class="logo hidden">
+            <img src="/free-html-5-1-1175208.webp"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="css-icon.webp"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="javascript-js-icon-2048x2048-nyxvtvk0.png"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="1280px-Sass_Logo_Color.svg.png"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="React-icon.svg.png"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="Typescript_logo_2020.svg.png"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="github.png"></img>
+          </div>
+          <div class="logo hidden">
+            <img src="Git-Icon-1788C.png"></img>
+          </div>
         </div>
       </section>
     </>
