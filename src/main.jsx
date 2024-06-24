fc429e2eb3524627c0fbd05f16fda3a19205c35d
Jaro <jaroslavfilo95@gmail.com>
2024-06-24 20:36:58 +0200

    i18n into src

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./Components/i18n.js";
+import "./i18n.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
